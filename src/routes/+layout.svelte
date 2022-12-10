854975efc3f5560cc3ed146979c25f1983db21c8
DreamTexX <kevin.cebulla@dreamtexx.email>
2022-12-10 21:24:15 +0100

    fix: background not scaling correctly

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,6 @@
 </script>
 
 <div class="min-h-screen bg-gradient-to-tr from-indigo-400 to-purple-300 flex justify-center items-center">
-    <img src={BackgroundPattern} alt="&nbsp;" class="fixed top-0 left-0 w-screen h-screen object-cover opacity-70">
+    <img src={BackgroundPattern} alt="&nbsp;" class="fixed top-0 left-0 w-screen h-screen bottom-0 right-0 object-cover opacity-70">
     <slot />
 </div>
